7be6f3b774827c14b9b37b69d323fb3c27c4df6e
2235443218a-has <2235443218@gamil.com>
2025-12-28 00:15:32 +0800

    feat: 实现评价回复功能，修复proto语法错误及gorm存储逻辑

--- internal/data/review.go
+++ internal/data/review.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"review-service/internal/biz"
 	"review-service/internal/data/model"
+	snowflake "review-service/pkg"
 
 	"github.com/go-kratos/kratos/v2/log"
 )
@@ -31,4 +32,22 @@ func (r *reviewRepo) GetReviewByOrderID(ctx context.Context, orderID int64) ([]*
 	reviews, err := r.data.query.ReviewInfo.WithContext(ctx).
 		Where(r.data.query.ReviewInfo.OrderID.Eq(orderID)).Find()
 	return reviews, err
+}
+func (r *reviewRepo) SaveReply(ctx context.Context, rpy *biz.ReplyParam) (*biz.ReplyParam, error) {
+
+	//数据校验 已经回复的不能回复，水平越权鉴定
+	rpy.ReplyID = snowflake.GenID()
+	info := &model.ReviewReplyInfo{
+		ReplyID:   rpy.ReplyID,
+		ReviewID:  rpy.ReviewID,
+		StoreID:   rpy.StoreID,
+		Content:   rpy.Content,
+		PicInfo:   rpy.PicInfo,
+		VideoInfo: rpy.VideoInfo,
+	}
+	if err := r.data.db.WithContext(ctx).Create(info).Error; err != nil {
+		r.log.Errorf("failed to create reply: %v", err) // 记录日志是个好习惯
+		return nil, err
+	}
+	return rpy, nil
 }
